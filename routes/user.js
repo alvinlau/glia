b165edf7b132f58885ce07032a08add1dd8078f1
Alvin Lau <alvinjasm@gmail.com>
2022-09-26 04:17:50 -0700

    check post user fields

--- routes/user.js
+++ routes/user.js
@@ -6,16 +6,20 @@ const MONGO_URL = 'mongodb+srv://glia:' + process.env.MONGO_PASSWD + '@cluster0.
 
 
 router.post('/', async function (req, res, next) {
-  // TODO sanitize fields
-  console.log(req.body)
-  const json = req.body
+  const {name, accessibility, price} = req.body
+  if ([name, accessibility, price].some(val => !val)) {
+    next('name, accessibility, price fields must be valid values')
+    return
+  }
+
+  const user = { name, accessibility, price }
 
   const mongo = new MongoClient(MONGO_URL)
   const users = mongo.db('bored').collection('users');
-  await users.insertOne(json)
+  await users.insertOne(user)
   await mongo.close()
 
-  res.send(json)
+  res.send(user)
 });
 
 module.exports = router;
