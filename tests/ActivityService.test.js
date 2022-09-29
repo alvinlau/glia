acfeac9f8606ce030cfab07b18e359d46e9c97f4
Alvin Lau <alvinjasm@gmail.com>
2022-09-29 01:52:42 -0700

    add more tests for getActivityWithUser

--- tests/ActivityService.test.js
+++ tests/ActivityService.test.js
@@ -1,6 +1,7 @@
 const ActivityService = require('../services/ActivityService')
 
 describe('ActivityService', () => {
+  const dbActivity = { _id: 'mongoid', activity: 'dbactivity' }
   const activities = {
     findOne: jest.fn(),
     insertOne: jest.fn(),
@@ -11,56 +12,67 @@ describe('ActivityService', () => {
     db: jest.fn().mockReturnValue({ collection: jest.fn().mockReturnValue(activities) }),
   }
   const activityService = new ActivityService(mongo)
-  const dbActivity = { _id: 'mongoid', activity: 'someactivity' }
-  const aggregateSpy = jest.spyOn(activities, 'aggregate').mockReturnValue({
-    next: jest.fn().mockReturnValue(dbActivity)
-  })
   const insertOneSpy = jest.spyOn(activities, 'insertOne').mockReturnValue(true)
 
   describe('getActivity', () => {
+    const aggregateSpy = jest.spyOn(activities, 'aggregate').mockReturnValue({
+      next: jest.fn().mockReturnValue(dbActivity)
+    })
     const boredActivitySpy = jest.spyOn(activityService, 'getActivityFromBored')
       .mockImplementation(() => { return { activity: 'bored' } })
 
     it('does not call bored API if an activity is found in db', async () => {
-      // const dbActivity = { _id: 'mongoid', activity: 'someactivity'}
-      // jest.spyOn(activities, 'aggregate').mockReturnValue({ 
-      //   next: jest.fn().mockReturnValue(dbActivity)
-      // })
       const activity = await activityService.getActivity()
-      expect(aggregateSpy).toBeCalled()
+      expect(aggregateSpy).toBeCalledWith(expect.arrayContaining([{ $sample: { size: 1 } }]))
       expect(boredActivitySpy).not.toBeCalled()
       expect(insertOneSpy).not.toBeCalled()
-      expect(activity).toMatchObject({ activity: 'someactivity' })
+      expect(activity).toMatchObject({ activity: 'dbactivity' })
     })
 
     it('calls bored API if no activity is found in db', async () => {
       jest.spyOn(activities, 'aggregate').mockReturnValue({ next: jest.fn().mockReturnValue(null) })
       const activity = await activityService.getActivity()
-      expect(aggregateSpy).toBeCalled()
+      expect(aggregateSpy).toBeCalledWith(expect.arrayContaining([{ $sample: { size: 1 } }]))
       expect(boredActivitySpy).toBeCalled()
       expect(insertOneSpy).toBeCalled()
       expect(activity).toMatchObject({ activity: 'bored' })
-    })    
+    })
   })
 
   describe('getActivityWithUser', () => {
     const boredActivitySpy = jest.spyOn(activityService, 'getActivityFromBored')
-      .mockImplementation(() => { return { activity: 'bored' } })
+      .mockReturnValue({ activity: 'bored', accessibility: 'SuperHigh', price: 'SuperLow' })
 
     it('does not call bored API if an activity is found in db', async () => {
       const mongoQuery = [
         { $match: { accessibility: 'SuperHigh', price: 'SuperLow' } },
         { $sample: { size: 1 } },
       ]
+      const aggregateSpy = jest.spyOn(activities, 'aggregate').mockReturnValue({
+        next: jest.fn().mockReturnValue(dbActivity)
+      })
 
       const activity = await activityService.getActivityWithUser({accessibility: 'SuperHigh', price: 'SuperLow'})
-      expect(aggregateSpy).toBeCalledWith(expect.arrayContaining([
-        expect.objectContaining({ $match: { accessibility: 'SuperHigh', price: 'SuperLow' } }),
-        expect.objectContaining({ $sample: { size: 1 } })
-      ]))
-      expect(boredActivitySpy).not.toBeCalled()
-      expect(insertOneSpy).not.toBeCalled()
-      expect(activity).toMatchObject({ activity: 'someactivity' })
+      expect(aggregateSpy).toBeCalledWith(expect.arrayContaining(mongoQuery))
+      expect(boredActivitySpy).toBeCalled() // trying to make this called 1 time
+      expect(insertOneSpy).toBeCalled() // trying to make this called 1 time
+      expect(activity).toMatchObject({ activity: 'dbactivity' })
+    })
+
+    it('calls bored API if no activity is found in db', async () => {
+      const mongoQuery = [
+        { $match: { accessibility: 'SuperHigh', price: 'SuperLow' } },
+        { $sample: { size: 1 } },
+      ]
+      const aggregateSpy = jest.spyOn(activities, 'aggregate').mockReturnValue({
+        next: jest.fn().mockReturnValue(null)
+      })
+
+      const activity = await activityService.getActivityWithUser({ accessibility: 'SuperHigh', price: 'SuperLow' })
+      expect(aggregateSpy).toBeCalledWith(expect.arrayContaining(mongoQuery))
+      expect(boredActivitySpy).toBeCalled() // trying to make this called 1 time
+      expect(insertOneSpy).toBeCalled() // trying to make this called 1 time
+      expect(activity).not.toMatchObject({ activity: 'dbactivity' })
     })
   })
 
